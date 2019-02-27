9439fa18146b99ccc74f612df233175b8c042552
sirga <luocaodan@163.com>
2019-02-27 15:59:43 +0800

    处理异常返回404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,17 @@ class ApplicationController < ActionController::Base
   before_action :require_login
   skip_before_action :verify_authenticity_token #, unless: csrf_required?
 
+  rescue_from Exception, with: :render_404
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  rescue_from ActionController::RoutingError, with: :render_404
+
   # def csrf_required?
   #   !request.format.json?
   # end
+
+  def render_404
+    render file: "#{Rails.root}/public/404.html", status: 404
+  end
 
   def require_login
     redirect_to login_url unless logged_in?
